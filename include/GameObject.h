7ad3ce23e830a830f21bbdf2d6ae2d522aa0cb96
No-needto-recall <111343528+No-needto-recall@users.noreply.github.com>
2023-05-18 15:53:12 +0800

    change GameObject.h

--- include/GameObject.h
+++ include/GameObject.h
@@ -1,37 +1,43 @@
 #pragma once
+
 #include <string>
 
-
 using std::string;
+
 
 //位置结构体
 struct Location {
     int x;
     int y;
+
+    // 转换为字符串的函数，便于记录日志
+    string ToString() const;
 };
 
-//图标
+//图标类型
 using Icon = char;
-
-
 
 class GameObject {
 public:
-    GameObject(const string& name, Location location, Icon icon);
+    GameObject(const Location& location, Icon icon, const string& name);
 
-    // Getters and Setters
-    Location getLocation() const;
-    void setLocation(Location newLocation);
+    // 获取物体的位置
+    Location GetLocation() const;
+    // 设置物体的位置
+    void SetLocation(const Location& newLocation);
 
-    Icon getIcon() const;
-    void setIcon(Icon newIcon);
+    // 获取物体的图标
+    Icon GetIcon() const;
+    // 设置物体的图标
+    void SetIcon(Icon newIcon);
 
-    string getName() const;
-    void setName(const string& newName);
+    // 获取物体的名称
+    string GetName() const;
+    // 设置物体的名称
+    void SetName(const string& newName);
 
 private:
-    string _name;
     Location _location;
     Icon _icon;
+    string _name;
 };
-
