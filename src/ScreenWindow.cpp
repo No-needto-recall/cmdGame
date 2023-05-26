517287122bdaee90a12bf540fed23efa60d914a3
ctl <ctl.com>
2023-05-26 16:28:48 +0800

    简单实现滚动窗口，但无法处理中文字符。 待优化utf-8编码。

--- src/ScreenWindow.cpp
+++ src/ScreenWindow.cpp
@@ -7,13 +7,14 @@ ScreenWindow::ScreenWindow(int x, int y, int width, int height)
 void ScreenWindow::addText(const std::string& str) {
     std::istringstream stream(str);
     std::string line;
-    while (std::getline(stream, line)) {
+    while (std::getline(stream, line,'\n')) {
         allLines.push_back(line);
     }
 }
 
 void ScreenWindow::displayNextPage() {
     if (allLines.empty()) {
+        refresh();
         return;
     }
 
@@ -24,29 +25,43 @@ void ScreenWindow::displayNextPage() {
         allLines.pop_front();
 
         while (line.length() > width) {
-            if (i >= height) {
-                allLines.push_front(line);  // 将未处理完的部分推回 allLines
-                return;
-            }
             lines.push_back(line.substr(0, width));
             line = line.substr(width);
             ++i;
+            // 到达页面最大行数，将未显示完全的行推回 allLines
+            if (i >= height) {
+                allLines.push_front(line);
+                refresh();
+                return;
+            }
         }
+
+        // 如果当前行在宽度允许的范围内，直接添加到 lines
         if (i < height) {
             lines.push_back(line);
             ++i;
-        }
-        else {
-            allLines.push_front(line);  // 如果高度已满，将剩余部分推回 allLines
         }
     }
     refresh();
 }
 
+
 void ScreenWindow::refresh() {
     ScreenDrawer& drawer = ScreenDrawer::getInstance(); // 获取ScreenDrawer的单例实例
     int y = topLeft.Y;
-    for (const auto& line : lines) {
-        drawer.drawString(topLeft.X, y++, line);
+    if (allLines.empty()&& lines.empty()) {
+        for (int i = 0; i < height; ++i) {
+            drawer.drawString(topLeft.X, y + i, std::string(width, ' '));
+        }
     }
+    else {
+		for (int i = 0; i < height; ++i) {
+            drawer.drawString(topLeft.X, y + i, std::string(width, ' '));
+        }
+        for (const auto& line : lines) {
+            drawer.drawString(topLeft.X, y++, line);
+        }
+    }
+    lines.clear();
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Game.h"
+#include "ScreenWindow.h"
 using namespace std;
 
 
 int main(int argc,char* argv[]) {
 
-	Game::getInstance().start();
+	ScreenWindow windows(0, 0, 10, 2);
+	windows.addText("my name is huawei,today is 5.12\n do u want to play game?");
+	while (1) {
+		windows.displayNextPage();
+		ScreenDrawer::getInstance().swapBuffers();
+		Sleep(3000);
+	}
+	//Game::getInstance().start();
 	return 0;
 }
